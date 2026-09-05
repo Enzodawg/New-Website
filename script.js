497040b846fd36401119ecd813f70d7fee43413a
Enzodawg <aryanprajapati6905@gmail.com>
2026-09-05 11:35:49 +0530

    Website

--- script.js
+++ script.js
@@ -1,8 +1,16 @@
-// Target Navbar elements
 const menuIcon = document.querySelector('#menu-icon');
 const navLinks = document.querySelector('.nav-links');
+const navItems = document.querySelectorAll('.nav-links a');
 
-// Toggle 'active' visibility rules on click configurations
-menuIcon.onclick = () => {
+// Toggle Mobile Menu
+menuIcon.addEventListener('click', () => {
     navLinks.classList.toggle('active');
-};
+});
+
+// Close Mobile Menu automatically on item click
+navItems.forEach(item => {
+    item.addEventListener('click', () => {
+        navLinks.classList.remove('active');
+    });
+});
+
